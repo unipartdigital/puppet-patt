d2ed48d1da912cd5a836692ef3f061e99da77fe4
Ali Bendriss <ali.bendriss@unipart.io>
2019-08-14 12:38:37 +0100

    add haproxy support

---
+++ dscripts/d40.haproxy.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+srcdir=$(cd $(dirname $0); pwd)
+# Exit the script on errors:
+set -e
+trap '{echo "$0 FAILED on line $LINENO!; rm -f $0 ;}" | tee ${srcdir}/$(basename $0).log' ERR
+# clean up on exit
+trap "{ rm -f $0; }" EXIT
+
+# Catch unitialized variables:
+set -u
+
+# arch | vendor | major
+get_system_release () {
+    query=$1
+    if [ "x$query" == "xarch" ]; then uname -m; return $?; fi
+    if [ -f /etc/redhat-release ]; then
+        release=$(rpm -q --whatprovides /etc/redhat-release)
+        case $query in
+            'major')
+                echo $release | rev |  cut -d '-' -f 2 | rev
+                ;;
+            'vendor')
+                echo $release | rev |  cut -d '-' -f 4 | rev
+                ;;
+            *)
+                echo "query not implemented: $query" 1>&2
+                exit 1
+        esac
+    fi
+}
+
+
+init() {
+    release_vendor=$(get_system_release "vendor")
+    release_major=$(get_system_release "major")
+    # release_arch=$(get_system_release "arch")
+
+    rel_epel="yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-${release_major}.noarch.rpm"
+    rpm_pkg="haproxy"
+
+    case "${release_vendor}" in
+        'redhat' | 'centos')
+            if [ "${release_major}" -lt 8 ]; then
+                yum install -y ${rel_epel}
+                yum install -y ${rpm_pkg}
+            else
+                dnf install -y ${rel_epel}
+                dnf install -y ${rpm_pkg}
+            fi
+            ;;
+        *)
+            echo "unsupported release vendor: ${release_vendor}" 1>&2
+            exit 1
+            ;;
+    esac
+}
+
+
+case "${1:-""}" in
+    'init')
+        shift 1
+        init $*
+        ;;
+    *)
+        echo "usage: $0 init"
+        exit 1
+        ;;
+esac
