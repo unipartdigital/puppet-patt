3ff413226ee09bfc9024a98ae2be90d16c2cbefe
Ali Bendriss <ali.bendriss@unipart.io>
2020-06-19 17:41:55 +0100

    fix add_repo
     should be a list of string

--- dscripts/d03.repo.sh
+++ dscripts/d03.repo.sh
@@ -38,25 +38,28 @@ logger () {
 }
 
 add_repo () {
-    repo_url=$*
+    repo_url="$*"
     release_vendor=$(get_system_release "vendor")
     release_major=$(get_system_release "major")
     release_arch=$(get_system_release "arch")
 
     case "${release_vendor}" in
         'redhat' | 'centos')
-            for r in ${repo_url[*]}; do
+            for r in "${repo_url[*]}"; do
+                if [ "x$r" == "x" ]; then
+                    continue
+                fi
                 if [ "${release_major}" -lt 8 ]; then
                     repo_name=$(echo "$r" | sed -e "s|https*://||" -e "s|[\.:/]|_|g" -e "s|\[|_|" -e "s|\]|_|" -e "s|_\+|_|g")
                     cat <<EOF > /etc/yum.repos.d/${repo_name}.repo
-[{repo_name}]
+[${repo_name}]
 name=created by $0 from ${r}
 baseurl=${r}
 enabled=1
 gpgcheck=0
 EOF
                 else
-                    dnf config-manager --nogpgcheck --add-repo ${r}
+                    dnf config-manager --nogpgcheck --add-repo "$r"
                 fi
             done
             ;;
