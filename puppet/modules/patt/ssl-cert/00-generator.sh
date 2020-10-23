a992bb28a779059a87dd437e0563053642b51b87
Ali Bendriss <ali.bendriss@unipart.io>
2020-10-23 01:39:06 +0100

    WIP puppet postgres user cert auth

--- puppet/modules/patt/ssl-cert/00-generator.sh
+++ puppet/modules/patt/ssl-cert/00-generator.sh
@@ -4,22 +4,41 @@ src_dir=$(cd $(dirname $0) && pwd)
 
 cluster_name=$1
 query=$2
+user=$3
 cert_dir=${src_dir}
+
+mk_ca_cert () {
+    ${cert_dir}/self_signed_certificate.py cli                        \
+               --ca_country_name           "UK"                       \
+               --ca_state_or_province_name "United Kingdom"           \
+               --ca_locality_name          "Cambridge"                \
+               --ca_organization_name      "Patroni Postgres Cluster" \
+               --ca_common_name            "CA ${cluster_name}"       \
+               --ca_not_valid_after        "3650"                     \
+               --ca_path                   "${cert_dir}/${cluster_name}_ca.pem" \
+               --ca_key_path               "${cert_dir}/${cluster_name}_ca.key"
+}
+
+mk_user_cert() {
+    ${cert_dir}/self_signed_certificate.py cli                                  \
+               --ca_path                   "${cert_dir}/${cluster_name}_ca.pem" \
+               --ca_key_path               "${cert_dir}/${cluster_name}_ca.key" \
+               --cert_country_name           "UK"                       \
+               --cert_state_or_province_name "United Kingdom"           \
+               --cert_locality_name          "Cambridge"                \
+               --cert_organization_name      "Patroni Postgres Cluster" \
+               --cert_common_name            "${user}"                  \
+               --cert_not_valid_after        "547"                      \
+               --cert_path                   "${cert_dir}/${cluster_name}_${user}.pem" \
+               --cert_key_path               "${cert_dir}/${cluster_name}_${user}.key"
+}
 
 case "$query" in
     'root_cert')
         if [ -f "${cert_dir}/${cluster_name}_ca.pem" ]; then
             /usr/bin/cat ${cert_dir}/${cluster_name}_ca.pem
         else
-            ${cert_dir}/self_signed_certificate.py cli                        \
-                       --ca_country_name           "UK"                       \
-                       --ca_state_or_province_name "United Kingdom"           \
-                       --ca_locality_name          "Cambridge"                \
-                       --ca_organization_name      "Patroni Postgres Cluster" \
-                       --ca_common_name            "CA ${cluster_name}"       \
-                       --ca_not_valid_after        "3650"                     \
-                       --ca_path                   "${cert_dir}/${cluster_name}_ca.pem" \
-                       --ca_key_path               "${cert_dir}/${cluster_name}_ca.key"
+            mk_ca_cert
             /usr/bin/cat ${cert_dir}/${cluster_name}_ca.pem
         fi
         ;;
@@ -27,16 +46,26 @@ case "$query" in
         if [ -f "${cert_dir}/${cluster_name}_ca.pem" ]; then
           /usr/bin/cat ${cert_dir}/${cluster_name}_ca.key
         else
-            ${cert_dir}/self_signed_certificate.py cli                        \
-                       --ca_country_name           "UK"                       \
-                       --ca_state_or_province_name "United Kingdom"           \
-                       --ca_locality_name          "Cambridge"                \
-                       --ca_organization_name      "Patroni Postgres Cluster" \
-                       --ca_common_name            "CA ${cluster_name}"       \
-                       --ca_not_valid_after        "3650"                     \
-                       --ca_path                   "${cert_dir}/${cluster_name}_ca.pem" \
-                       --ca_key_path               "${cert_dir}/${cluster_name}_ca.key"
+            mk_ca_cert
             /usr/bin/cat ${cert_dir}/${cluster_name}_ca.key
+        fi
+        ;;
+    'user_cert')
+        if [ -f "${cert_dir}/${cluster_name}_${user}.pem" ]; then
+            /usr/bin/cat ${cert_dir}/${cluster_name}_${user}.pem
+        else
+            if [ ! -f "${cert_dir}/${cluster_name}_ca.pem" ]; then mk_ca_cert; fi
+            mk_user_cert
+            /usr/bin/cat ${cert_dir}/${cluster_name}_${user}.pem
+        fi
+        ;;
+    'user_key')
+        if [ -f "${cert_dir}/${cluster_name}_${user}.key" ]; then
+            /usr/bin/cat ${cert_dir}/${cluster_name}_${user}.key
+        else
+            if [ ! -f "${cert_dir}/${cluster_name}_ca.pem" ]; then mk_ca_cert; fi
+            mk_user_cert
+            /usr/bin/cat ${cert_dir}/${cluster_name}_${user}.key
         fi
         ;;
     'server_cert')
