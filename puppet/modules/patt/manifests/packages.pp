0c4e4f419522fe7632d02d4108adc6b533b7cc7a
Ali Bendriss <ali.bendriss@unipart.io>
2021-01-06 10:50:20 +0000

    let patt install etcd and postgres
     installing etcd and postgres via puppet pose some problems:
      - install delayed until the agent run on each node
      - need more code to do a conditional install (etcd node / data node)
      - don't want to have a yaml definition for each node (symlink is fine)

--- puppet/modules/patt/manifests/packages.pp
+++ puppet/modules/patt/manifests/packages.pp
@@ -15,31 +15,31 @@ class patt::packages()
  package { $base: ensure => 'installed' }
  package { $pyth: ensure => 'installed' }
 
- package{'pgdg-redhat-repo':
-  provider => 'dnf',
-  ensure => 'present',
-  source => 'https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm',
- }
+#  package{'pgdg-redhat-repo':
+#   provider => 'dnf',
+#   ensure => 'present',
+#   source => 'https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm',
+#  }
 
- Exec{'/etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo':
-  command  => '/usr/bin/curl https://copr.fedorainfracloud.org/coprs/unipartdigital/pkgs/repo/epel-8/unipartdigital-pkgs-epel-8.repo > /etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo',
-  unless => '/bin/test -f /etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo'
-  }
+#  Exec{'/etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo':
+#   command  => '/usr/bin/curl https://copr.fedorainfracloud.org/coprs/unipartdigital/pkgs/repo/epel-8/unipartdigital-pkgs-epel-8.repo > /etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo',
+#   unless => '/bin/test -f /etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo'
+#   }
 
- package {'etcd': ensure  => 'installed',
-                  require => Exec['/etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo'],
-                  install_only => true}
+#  package {'etcd': ensure  => 'installed',
+#                   require => Exec['/etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo'],
+#                   install_only => true}
 
- exec { 'dnf_module_disable_postgresql':
-    command => "/usr/bin/dnf -qy module disable postgresql",
-    require => Package['pgdg-redhat-repo'],
- }
+#  exec { 'dnf_module_disable_postgresql':
+#     command => "/usr/bin/dnf -qy module disable postgresql",
+#     require => Package['pgdg-redhat-repo'],
+#  }
 
- $pg_pkg = [ "postgresql${patt::postgres_release}", "postgresql${patt::postgres_release}-server", "postgresql${patt::postgres_release}-contrib" ]
- package { $pg_pkg: ensure => 'installed', require => Exec['dnf_module_disable_postgresql'] }
+#  $pg_pkg = [ "postgresql${patt::postgres_release}", "postgresql${patt::postgres_release}-server", "postgresql${patt::postgres_release}-contrib" ]
+#  package { $pg_pkg: ensure => 'installed', require => Exec['dnf_module_disable_postgresql'] }
 
-# TODO
-# conditional
-# 'haproxy'
+# # TODO
+# # conditional
+# # 'haproxy'
 
 }
