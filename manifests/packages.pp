f883c54de821dcd6cee23dc36272d503a0fdf875
Ali Bendriss <ali.bendriss@unipart.io>
2021-02-05 03:42:33 +0000

    1612496553

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,29 +3,41 @@ class patt::packages()
 
  # pre
  # needed by epel-release
- yumrepo { 'PowerTools':
-   enabled  => 1,
-   gpgcheck => '1',
+ unless defined(Yumrepo["PowerTools"]) {
+  yumrepo { 'PowerTools':
+    enabled  => 1,
+    gpgcheck => '1',
+  }
  }
 
  $base = ['epel-release', 'gcc', 'make', 'nftables', 'policycoreutils', 'util-linux', 'xfsprogs', 'lvm2', 'cryptsetup', 'psmisc']
 
  $pyth = ['python3', 'python3*-scapy', 'python3*-Cython', 'python3*-PyYAML', 'python3*-devel', 'python3*-pip', 'python3*-psycopg2']
 
- package { $base: ensure => 'installed' }
- package { $pyth: ensure => 'installed' }
-
- package{'pgdg-redhat-repo':
-  provider => 'dnf',
-  ensure => 'present',
-  source => 'https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm',
+ $base.each|$b| {
+  unless defined(Package["$b"]) {
+   package { $b: ensure => 'installed' }
+  }
  }
 
+ $pyth.each|$p| {
+  unless defined(Package["$p"]) {
+   package { $p: ensure => 'installed' }
+  }
+ }
+
+ unless defined(Package["pgdg-redhat-repo"]) {
+  package{'pgdg-redhat-repo':
+   provider => 'dnf',
+   ensure => 'present',
+   source => 'https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm',
+  }
+ }
 
 if "${is_etcd}" == "true" {
  notify {"etcd peer install":}
  Exec{'/etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo':
-  command  => '/usr/bin/curl https://copr.fedorainfracloud.org/coprs/unipartdigital/pkgs/repo/epel-8/unipartdigital-pkgs-epel-8.repo > /etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo',
+  command  => '/usr/bin/curl -f https://copr.fedorainfracloud.org/coprs/unipartdigital/pkgs/repo/epel-8/unipartdigital-pkgs-epel-8.repo > /etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo',
   unless => '/bin/test -f /etc/yum.repos.d/unipartdigital-pkgs-epel-8.repo'
   }
 
@@ -43,7 +55,11 @@ if "${is_postgres}" == "true" {
  }
 
  $pg_pkg = [ "postgresql${patt::postgres_release}", "postgresql${patt::postgres_release}-server", "postgresql${patt::postgres_release}-contrib" ]
- package { $pg_pkg: ensure => 'installed', require => Exec['dnf_module_disable_postgresql'] }
+ $pg_pkg.each|$p| {
+  unless defined(Package["$p"]) {
+   package { $p: ensure => 'installed', require => Exec['dnf_module_disable_postgresql'] }
+  }
+ }
 }
 
 # # TODO
