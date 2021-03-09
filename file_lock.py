dc5745b9a7ba74ccdd84c947a4e00bea0ff7b0f1
Ali Bendriss <ali.bendriss@unipart.io>
2021-03-09 11:00:44 +0000

    don't lock if no process

--- file_lock.py
+++ file_lock.py
@@ -20,22 +20,40 @@ with lock:
     time.sleep(360)
 
 """
+def is_running(pid):
+    if pid and os.path.isdir('/proc/{}'.format(pid)):
+        return True
+    return False
 
 class file_lock(object):
     class file_lock_exception (Exception):
         pass
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, pid=os.getpid()):
         self.is_locked = False
         self.lockfile = file_name
+        self.pid = pid
 
     def aquire (self):
         try:
             fd = os.open( self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR )
             with os.fdopen( fd, 'a' ) as f:
-                f.write('')
+                f.write("{}".format(self.pid))
         except FileExistsError:
-            raise file_lock.file_lock_exception
+            with open(self.lockfile) as pid_file:
+                pid = pid_file.read().replace('\n', '')
+                if is_running (int(pid)):
+                    raise file_lock.file_lock_exception
+            os.unlink(self.lockfile)
+            try:
+                fd = os.open( self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR )
+                with os.fdopen( fd, 'a' ) as f:
+                    f.write("{}".format(self.pid))
+            except FileExistsError:
+                raise file_lock.file_lock_exception
+            else:
+                self.is_locked = True
+                return True
         else:
             self.is_locked = True
             return True
