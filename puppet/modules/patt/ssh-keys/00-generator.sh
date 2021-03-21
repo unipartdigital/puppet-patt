f66b66def9da705eadc8ced36cb988957c910225
Ali Bendriss <ali.bendriss@unipart.io>
2021-03-21 23:53:30 +0000

    fix: ssh-keygen -q

--- puppet/modules/patt/ssh-keys/00-generator.sh
+++ puppet/modules/patt/ssh-keys/00-generator.sh
@@ -8,7 +8,7 @@ case "$keytype" in
     'private')
         /usr/bin/mkdir -m 700 -p ${keydir}
         if [ ! -f "${keydir}/${cluster_name}_rsa" ]; then
-            /usr/bin/ssh-keygen -t rsa -b 4096 -f ${keydir}/${cluster_name}_rsa -N "" -C ${cluster_name}
+            /usr/bin/ssh-keygen -q -t rsa -b 4096 -f ${keydir}/${cluster_name}_rsa -N "" -C ${cluster_name}
             /usr/bin/cat ${keydir}/${cluster_name}_rsa
         else
             /usr/bin/cat ${keydir}/${cluster_name}_rsa
@@ -17,7 +17,7 @@ case "$keytype" in
     'public')
         /usr/bin/mkdir -m 700 -p ${keydir}
         if [ ! -f "${keydir}/${cluster_name}_rsa.pub" ]; then
-            /usr/bin/ssh-keygen -t rsa -b 4096 -f ${keydir}/${cluster_name}_rsa -N "" -C ${cluster_name}
+            /usr/bin/ssh-keygen -q -t rsa -b 4096 -f ${keydir}/${cluster_name}_rsa -N "" -C ${cluster_name}
             /usr/bin/cat ${keydir}/${cluster_name}_rsa.pub
         else
             /usr/bin/cat ${keydir}/${cluster_name}_rsa.pub
