1397b9351d19e6a8c02b5d85eaa165b828efd049
Ali Bendriss <ali.bendriss@unipart.io>
2021-06-25 09:53:33 +0100

    stats_temp_directory
     systemd
     TemporaryFileSystem=/var/cache/pg_stats_temp
     allow to mount a tmpfs into /var/cache/pg_stats_temp
    
     could be used by postgres by setting in config/postgres-ipv6.yaml
     stats_temp_directory: /var/cache/pg_stats_temp

--- dscripts/d30.patroni.sh
+++ dscripts/d30.patroni.sh
@@ -88,7 +88,7 @@ After=network.target
 Type=simple
 User=postgres
 Group=postgres
-TemporaryFileSystem=/var/cache/pg_stats_temp:nodev,strictatime,mode=0750
+TemporaryFileSystem=/var/cache/pg_stats_temp:nodev,strictatime,mode=0750,uid=`id -u postgres`,gid=`id -g postgres`
 Environment=PGDATA=${postgres_home}/${postgres_version}/data/
 Environment=PATH=${postgres_bin}:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 OOMScoreAdjust=-1000
