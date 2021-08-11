08fa73945d96ba7e23a758cfca5205bc7b4124da
Ali Bendriss <ali.bendriss@unipart.io>
2021-08-11 01:19:05 +0100

    container name validation
     basic S3 container name validation
     improve the message on BucketAlreadyExists exception
    
    	modified:   dscripts/create_bucket.py

--- dscripts/create_bucket.py
+++ dscripts/create_bucket.py
@@ -20,6 +20,26 @@ container=args.bucket
 profile = args.aws_profile
 region = args.aws_region
 
+lowercase_letter_or_number="abcdefghijklmnopqrstuvwxyz0123456789"
+allowed_chars = lowercase_letter_or_number + ".-"
+assert [x for x in container if x not in allowed_chars] == [], "Bucket names can consist only of lowercase letters, numbers, dots (.), and hyphens (-)"
+assert len(container) >= 3 and len(container) <= 63, "Bucket names must be between 3 and 63 characters long"
+assert container[0] in lowercase_letter_or_number and container[-1] in lowercase_letter_or_number, "Bucket names must begin and end with a letter or number"
+assert ".." not in container, "The bucket name cannot have consecutive periods"
+assert ".-" not in container and "-." not in container, "The bucket name cannot use hyphens (-) adjacent to periods"
+
+try:
+    import ipaddress
+except ModuleNotFoundError:
+    pass
+else:
+    try:
+        ipaddress.ip_address(container)
+    except ValueError:
+        pass
+    else:
+        assert False, "Bucket names must not be formatted as an IP address"
+
 session = boto3.Session(profile_name=profile, region_name=region)
 
 if args.aws_force_path:
@@ -31,6 +51,9 @@ try:
     s3.create_bucket(Bucket=container)
 except s3.exceptions.BucketAlreadyOwnedByYou:
     pass
+except s3.exceptions.BucketAlreadyExists as e:
+    e.args += ("Bucket names must be unique within a partition", )
+    raise
 except Exception as e:
     raise
 
